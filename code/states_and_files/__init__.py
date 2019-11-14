70d97df4d34a4485c3f37eba2b5f70dbdfcee9c9
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2019-11-14 11:38:57 -0800

    cosmetic improvement

--- code/states_and_files/__init__.py
+++ code/states_and_files/__init__.py
@@ -15,6 +15,8 @@ class State:
     
 def create_state(abbr):
     if abbr == 'NC':
+        name='North Carolina'
+        db_name='nc'
         nc_meta_p=re.compile(r"""
         (?P<field>[^\n\t]+)
         \t+
@@ -25,7 +27,7 @@ def create_state(abbr):
         nc_type_map = {'number':'INT', 'text':'varchar', 'char':'varchar'}
         nc_path_to_data = "local_data/NC/data/"
         nc_correction_query_list = ['ALTER TABLE results_pct ALTER COLUMN precinct SET DATA TYPE varchar(23)']  #metadata says precinct field has at most 12 characters but 'ABSENTEE BY MAIL 71-106' has 13
-        return State("NC","North Carolina",nc_meta_p,nc_type_map,"nc",nc_path_to_data,nc_correction_query_list)
+        return State(abbr,name,nc_meta_p,nc_type_map,db_name,nc_path_to_data,nc_correction_query_list)
     else:
         return('Error: "'+abbr+'" is not a state abbreviation recognized by the code.')
         sys.exit()
