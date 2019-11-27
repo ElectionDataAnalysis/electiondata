0fec451bae9e0750ff98e6221c217a2b83c5e165
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2019-11-26 17:59:24 -0800

    store enumerations in separate folder

--- code/SQL/NC_results_to_CDF.sql
+++ code/SQL/NC_results_to_CDF.sql
@@ -3,19 +3,22 @@
 
 DROP TABLE IF EXISTS CountItemStatus;
 CREATE TABLE CountItemStatus (
-id NUMERIC NOT NULL,
+id SERIAL,
 text varchar(13),
 CONSTRAINT countitemstatus_pk PRIMARY KEY(id)
 )
 ;
 
-INSERT INTO CountItemStatus (id,text)
-VALUES
-(1,'completed'),
-(2,'in-process'),
-(3,'not-processed'),
-(4,'unknown')
+COPY CountItemStatus (text) FROM '/container_root_dir/SQL/enumerations/CountItemStatus.txt';
+
+DROP TABLE IF EXISTS ReportingUnitType;
+CREATE TABLE ReportingUnitType (
+id SERIAL,
+text varchar(20),
+CONSTRAINT ReportingUnitType_pk PRIMARY KEY(id)
+)
 ;
+
 
 
 
