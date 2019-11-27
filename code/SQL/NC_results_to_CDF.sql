efe73ff7da29d653cbd4bad6a5be522f915bd077
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2019-11-26 19:05:00 -0800

    start to add party table

--- code/SQL/NC_results_to_CDF.sql
+++ code/SQL/NC_results_to_CDF.sql
@@ -21,12 +21,20 @@ CONSTRAINT ReportingUnitType_pk PRIMARY KEY(id)
 
 COPY ReportingUnitType (text) FROM '/container_root_dir/SQL/enumerations/ReportingUnitType.txt';
 
-
-
 DROP TABLE IF EXISTS ReportingUnit;
 CREATE TABLE ReportingUnit (
 
 )
 ;
 
+DROP TABLE IF EXISTS Party;
+CREATE TABLE Party (
+id SERIAL,
+abbreviation varchar(6),
+name varchar(50),
+CONSTRAINT Party_pk PRIMARY KEY (id)
+)
+;
 
+-- BTRIM removes both leading and trailing spaces from a string
+INSERT INTO Party (abbreviation) SELECT DISTINCT BTRIM(choice_party) FROM results_pct WHERE choice_party IS NOT NULL AND BTRIM(results_pct.choice_party) <> '';
