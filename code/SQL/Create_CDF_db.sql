1b5d9c56daacf0a2812cf30dc09d06e28bb760f5
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2019-11-29 20:10:27 -0800

    'add uniqueness constraints to cdf schema'

--- code/SQL/Create_CDF_db.sql
+++ code/SQL/Create_CDF_db.sql
@@ -7,14 +7,14 @@ CREATE SEQUENCE cdf.id_seq   -- helps create unique id value across all tables i
 DROP TABLE IF EXISTS cdf.IdentifierType;
 CREATE TABLE cdf.IdentifierType (
 id BIGINT DEFAULT nextval('cdf.id_seq') PRIMARY KEY,
-text varchar(30)
+text varchar(30) UNIQUE
 );
 COPY cdf.IdentifierType (text) FROM '/container_root_dir/SQL/enumerations/IdentifierType.txt';
 
 DROP TABLE IF EXISTS cdf.GPUnit;
 CREATE TABLE cdf.GPUnit (
 id BIGINT DEFAULT nextval('cdf.id_seq') PRIMARY KEY,
-name varchar(50)
+name varchar(50) UNIQUE
 );
 
 DROP TABLE IF EXISTS cdf.ExternalIdentifier;
@@ -23,21 +23,22 @@ id BIGINT DEFAULT nextval('cdf.id_seq') PRIMARY KEY,
 foreign_id INTEGER, 
 value varchar(50),
 identifiertype_id INTEGER REFERENCES cdf.identifiertype(id),
-othertype varchar(30)
+othertype varchar(30),
+UNIQUE (foreign_id,identifiertype_id,othertype)
 );
 
 
 DROP TABLE IF EXISTS cdf.CountItemStatus;
 CREATE TABLE cdf.CountItemStatus (
 id BIGINT DEFAULT nextval('cdf.id_seq') PRIMARY KEY,
-text varchar(13)
+text varchar(13) UNIQUE
 );
 COPY cdf.CountItemStatus (text) FROM '/container_root_dir/SQL/enumerations/CountItemStatus.txt';
 
 DROP TABLE IF EXISTS cdf.ReportingUnitType;
 CREATE TABLE cdf.ReportingUnitType (
 id BIGINT DEFAULT nextval('cdf.id_seq') PRIMARY KEY,
-text varchar(30)
+text varchar(30) UNIQUE
 );
 COPY cdf.ReportingUnitType (text) FROM '/container_root_dir/SQL/enumerations/ReportingUnitType.txt';
 
@@ -46,30 +47,29 @@ CREATE TABLE cdf.ReportingUnit (
 id BIGINT DEFAULT nextval('cdf.id_seq') PRIMARY KEY,
 reportingunittype_id INTEGER REFERENCES cdf.reportingunittype(id),
 othertype varchar(30),
-countitemstatus_id INTEGER REFERENCES cdf.countitemstatus(id)
+countitemstatus_id INTEGER REFERENCES cdf.countitemstatus(id),
+gpunit_id INTEGER REFERENCES cdf.gpunit(id)
 );
-
 
 DROP TABLE IF EXISTS cdf.Party;
 CREATE TABLE cdf.Party (
 id BIGINT DEFAULT nextval('cdf.id_seq') PRIMARY KEY,
 abbreviation varchar(6),
-name varchar(50)
+name varchar(50) UNIQUE
 )
 ;
-
 
 DROP TABLE IF EXISTS cdf.ElectionType;
 CREATE TABLE cdf.ElectionType (
 id BIGINT DEFAULT nextval('cdf.id_seq') PRIMARY KEY,
-text varchar(30)
+text varchar(30) UNIQUE
 );
 COPY cdf.ElectionType (text) FROM '/container_root_dir/SQL/enumerations/ElectionType.txt';
 
 DROP TABLE IF EXISTS cdf.Election;
 CREATE TABLE cdf.Election (
 id BIGINT DEFAULT nextval('cdf.id_seq') PRIMARY KEY,
-name varchar(50),
+name varchar(50) UNIQUE,
 enddate DATE,
 startdate DATE,
 electiontype_id INTEGER REFERENCES cdf.electiontype(id),
