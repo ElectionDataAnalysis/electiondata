b5a8da6aebe16984f261fccfdc9b37aea0a62ae3
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2019-12-27 10:57:53 -0800

    start using setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="ElectionAnomalyDetection",
+    version="0.1",
+    packages=find_packages(),
+    url="https://github.com/sfsinger19103/results_analysis",
+    author="Stephanie Singer",
+    author_email="sfsinger@campaignscientific.com",
+
+)
