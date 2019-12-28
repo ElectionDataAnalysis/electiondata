2d7f08e138f128ea6dd303411340fbd1cd7b4296
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2019-12-28 15:14:19 -0800

    cleanup main module; raw_data function needs work

--- src/election_anomaly/__init__.py
+++ src/election_anomaly/__init__.py
@@ -73,46 +73,38 @@ def create_schema(s):
     if con:
         con.close()
 
-def raw_data(s,datafileinfo):
-    """ datafileinfo is a list of eleciton-file pairs, e.g., [['General Election 2018-11-06','results_pct_20181106.txt']]
+def raw_data(df,con,cur):
+    """ Loads the raw data from the df into the schema for the associated state
     """
 
-   # instantiate the datafiles
-    datafiles = [ sf.create_datafile(s,*x,{}) for x in datafileinfo]
-
+    s = df.state
     # create the schema for the state
     create_schema(s)
 
-    # connect to the state schema
-    print('Connect to database')
-    con = establish_connection()
-    cur = con.cursor()
+    t = df.table_name   # name of table
+    e = df.metafile_encoding
+    fpath = s.path_to_state_dir + 'meta/'+df.metafile_name
+    check_args(s,fpath,t)   # checking s is redundant ***
 
-    for df in datafiles:
-        t = df.table_name   # name of table
-        e = df.metafile_encoding
-        fpath = s.path_to_state_dir + 'meta/'+df.metafile_name   # this path is outside the docker container.
-        check_args(s,fpath,t)   # checking s is redundant
+    # clean the metadata file
+    fpath = cl.extract_first_col_defs(fpath,'local_data/tmp/',e)
 
-        # clean the metadata file
-        fpath = cl.extract_first_col_defs(fpath,'local_data/tmp/',e)
+    # create table and commit
 
-        # create table and commit
+    cur.execute(sql.SQL('DROP TABLE IF EXISTS {}.{}').format(sql.Identifier(s.schema_name),sql.Identifier(t)))
+    [query,strs,sql_ids] = dbr.create_table(df)
+    format_args = [sql.Identifier(x) for x in sql_ids]
+    cur.execute(sql.SQL(query).format( *format_args ),strs)
+    con.commit()
 
-        cur.execute(sql.SQL('DROP TABLE IF EXISTS {}.{}').format(sql.Identifier(s.schema_name),sql.Identifier(t)))
-        [query,strs,sql_ids] = dbr.create_table(df)
-        format_args = [sql.Identifier(x) for x in sql_ids]
-        cur.execute(sql.SQL(query).format( *format_args ),strs)
+    # correct any errors due to foibles of particular datafile and commit
+    for query in df.correction_query_list:
+        cur.execute(sql.SQL(query).format(sql.Identifier(s.schema_name), sql.Identifier(df.table_name)))
+        print(query)
         con.commit()
 
-        # correct any errors due to foibles of particular datafile and commit
-        for query in df.correction_query_list:
-            cur.execute(sql.SQL(query).format(sql.Identifier(s.schema_name), sql.Identifier(df.table_name)))
-            print(query)
-            con.commit()
-
-    # load data into tables
-        dbr.load_data(con,cur,s,df)
+# load data into tables
+    dbr.load_data(con,cur,s,df)
 
     # close connection
     if cur:
@@ -120,80 +112,26 @@ def raw_data(s,datafileinfo):
     if con:
         con.close()
     return
+
         
-        
-def file_to_context():
-# initialize report for logging
-    rs=[str(datetime.now())]
-# instantiate state of NC
-    s = sf.create_state('NC','local_data/NC')
-# instantiate the NC pct_result datafile
-    df = sf.create_datafile(s,'General Election 2018-11-06','results_pct_20181106.txt',{})
-# instantiate the 'nc_export1' munger
-    m = sf.create_munger('local_data/mungers/nc_export1.txt')
-    
-# connect to db
-    con = establish_connection()
-    cur = con.cursor()
-    
+def file_to_context(s,df,m,con,cur):
+    """ s is a state, df is a datafile, m is a munger """
+    print(str(datetime.now())
+
     [munger_d,munger_inverse_d] = context.build_munger_d(df.state,m)
-
     a = context.raw_to_context(df,m,munger_d,con,cur)
-    rs.append(str(a))
+    print(str(a))
+    return
 
-    return("<p>"+"</p><p>  ".join(rs))
-
-def create_cdf():
-    from db_routines import Create_CDF_db as CDF
-    rs =[str(datetime.now())]
-    con = establish_connection()
-    cur = con.cursor()
-    rs.append('Connected to database')
-    a = CDF.create_common_data_format_schema(con,cur,'cdf2')
-    rs.append(str(a))
-    
-    if cur:
-        cur.close()
-    if con:
-        con.close()
-    return("<p>"+"</p><p>  ".join(rs))
-
-# close connection
-    rs.append('Close connection')
-    if cur:
-        cur.close()
-    if con:
-        con.close()
-    return("<p>"+"</p><p>  ".join(rs))
-
-def load_cdf():
-    rs=[str(datetime.now())]
-    # instantiate state of NC, munger
-    rs.append('Create NC')
-    s = sf.create_state('NC','local_data/NC')
-    rs.append('Create munger')
-    m = sf.create_munger('local_data/mungers/nc_export1.txt')
-    
-    rs.append('Connect to db')
-    con = establish_connection()
-    cur = con.cursor()
-
-    rs.append('Load information from context dictionary for '+s.name)
-    context_to_db_d = context.context_to_cdf(s,'cdf2',con,cur)  # {'ReportingUnit':{'North Carolina':59, 'North Carolina;Alamance County':61} ... }
+def load_cdf(s,schema_name,con,cur):
+    print('Load information from context dictionary for '+s.name)
+    context_to_db_d = context.context_to_cdf(s,schema_name,con,cur)  # {'ReportingUnit':{'North Carolina':59, 'North Carolina;Alamance County':61} ... }
     con.commit()
-    
 
     ## load data from state raw schema
-    rs.append('NOT YET CODED: Load info from records in schema '+s.schema_name+' into CDF schema')
+    print('NOT YET CODED: Load info from records in schema '+s.schema_name+' into CDF schema')
     ## *** to do
-
-# close connection
-    rs.append('Close connection')
-    if cur:
-        cur.close()
-    if con:
-        con.close()
-    return("<p>"+"</p><p>  ".join(rs))
+    return
 
 
 
@@ -201,32 +139,41 @@ def load_cdf():
 
 from munge_routines import  format_type_for_insert
 
-def gui():
+def gui(state_abbr,path_to_state_dir,cdf_schema_name,munger_path,df_election,df_name):
+    """ state_abbr: e.g., 'NC'. path_to_state_dir: e.g., 'local_data/NC'
+    munger_path: e.g., 'local_data/mungers/nc_export1.txt'
+    df_election: e.g. 'General Election 2018-11-06','results_pct_20181106.txt',
+    df_name: e.g. 'General Election 2018-11-06','results_pct_20181106.txt'
+    *** need df_election to be a key in the state's Election.txt file. """
     from munge_routines import nc_export1
     from munge_routines import upsert
     con = establish_connection()
     cur = con.cursor()
-    # instantiate state of NC
-    s = sf.create_state('NC','local_data/NC')
-    # instantiate the 'nc_export1' munger
+
+    print('Instantiating the state of '+state_abbr)
+    s = sf.create_state(state_abbr,path_to_state_dir)
 
     # create cdf schema
-    print('Creating CDF schema')
-    CDF.create_common_data_format_schema(con, cur, 'cdf2')
-    # load state context info into cdf schema
-    print('Loading state context info into CDF schema') # *** takes a long time; why?
-    context.context_to_cdf(s,'cdf2',con,cur)
+    print('Creating CDF schema '+cdf_schema_name)
+    CDF.create_common_data_format_schema(con, cur, cdf_schema_name)
+
+    print('Loading state context info into CDF schema '+cdf_schema_name) # *** takes a long time; why?
+    context.context_to_cdf(s,cdf_schema_name,con,cur)
     con.commit()
-    print('Creating munger instance')
-    m = sf.create_munger('local_data/mungers/nc_export1.txt')
+
+    print('Creating munger instance from '+munger_path)
+    m = sf.create_munger(munger_path)
     # instantiate the NC pct_result datafile
 
     print('Creating datafile instance')
-    df = sf.create_datafile(s,'General Election 2018-11-06','results_pct_20181106.txt',m)
+    df = sf.create_datafile(s,df_election,df_name,m)
+
+    print('Load raw data from datafile ' + df_name + ' into schema '+s.schema_name)
+    raw_data(s, df)
 
     # load data from df to CDF schema (assumes already loaded to schema s.schema_name)
-    print('Loading data from df to CDF schema')
-    nc_export1.raw_records_to_cdf(df,'cdf2',con,cur)
+    print('Loading data from df to CDF schema '+cdf_schema_name)
+    nc_export1.raw_records_to_cdf(df,cdf_schema_name,con,cur)
     print('Done!')
 
 
@@ -239,11 +186,7 @@ def gui():
 
 if __name__ == '__main__':
 
-    s = sf.create_state('XX','local_data/XX/')
-    raw_data(s, [['General Election 2018-11-06', 'mini.txt']])
-
     from munge_routines import nc_export1
-    from munge_routines import upsert
 
     con = establish_connection()
     cur = con.cursor()
