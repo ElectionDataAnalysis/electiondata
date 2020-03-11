c6aea26da59ebd816d0027a3e933803e3cd2c46e
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2020-03-11 15:38:22 -0700

    start coding creation of new state

--- src/election_anomaly/user_interface/__init__.py
+++ src/election_anomaly/user_interface/__init__.py
@@ -2,25 +2,47 @@
 import db_routines as dbr
 import pandas as pd
 from sqlalchemy.orm import sessionmaker
+import os
+import states_and_files as sf
 
 
-def pick_one(df,item='row'):
+def pick_one(df,return_col,item='row'):
 	"""Returns index of item chosen by user"""
-	# TODO check that index entries are ints (or handle non-int)
+	# TODO check that index entries are positive ints (and handle error)
 	print(df)
-	choice = int(input(f'Enter the {df.index.name} of the desired {item} (or hit return if none is correct):\n'))
-	if choice == '':
-		return None
+	index_name = df.index.name or 'row number'
+	choice_str = input(f'Enter the {index_name} of the desired {item} '
+						   f'(or nothing if none is correct):\n')
+	if choice_str == '':
+		return None,None
 	else:
+		choice = -1	# not in df.index
 		while choice not in df.index:
-			choice = input('Entry must be in {}. Please try again.'.format(df.index))
-		return choice
+			choice_str = input(f'Entry must be in {index_name} column. Please try again.\n')
+			try:
+				choice = int(choice_str)
+			except ValueError:
+				choice_str = input(f'You must enter a number (or nothing), then hit return.\n')
+
+		return choice, df.loc[choice,return_col]
 
 
 def pick_schema(paramfile):
 	# TODO tell user to choose desired db via paramfile
 	con = dbr.establish_connection(paramfile=paramfile)  # TODO error handling for paramfile
+	print(f'Connection established to database {con.info.dbname}')
 	cur = con.cursor()
+	db_df = pd.DataFrame(dbr.query('SELECT datname FROM pg_database',[],[],con,cur))
+
+	db_idx,desired_db = pick_one(db_df,0,item='database')
+	if desired_db != con.info.dbname:
+		cur.close()
+		con.close()
+		con = dbr.establish_connection(paramfile,db_name=desired_db)
+		cur = con.cursor()
+
+	schema_df = pd.DataFrame(dbr.query('SELECT schema_name FROM information_schema.schemata',[],[],con,cur))
+	schema_idx,schema = pick_one(schema_df,0,item='schema')
 
 	# TODO show user list of schemas existing in db
 	# TODO if new, create new schema with all the necessary cdf tables
@@ -40,14 +62,49 @@ def pick_election(session,schema):
 	return election
 
 
-def pick_state(path_to_state_dir='../local_data/'):
+def pick_state(path_to_states='../local_data/'):
+	"""Returns a State object"""
+	if path_to_states[-1] != '/': path_to_states += '/'
+	state_df = pd.DataFrame(os.listdir(path_to_states),columns='State')
+	state_idx,state_name = pick_one(state_df,'State', item='state')
+	if state_idx != None:
+		# initialize the state
+		spath = f'{path_to_states}{state_name}'
+		ss = sf.State(state_name,spath)
+	else:
+		ss = create_state(path_to_states)
 	# TODO if no state chosen, return None
-	return state
+	return ss
 
 
-def create_state():
-	# TODO walk user through state creation
-	return state
+def create_state(parent_path):
+	"""walk user through state creation, return State object"""
+	# TODO
+	state_name = input('Enter a short name (alphanumeric only, no spaces) for your state (e.g., \'NC\')')
+	# TODO check alphanumeric only
+	state_path = f'{parent_path}{state_name}/'
+
+	# create state directory
+	try:
+		os.mkdir(state_path)
+	except FileExistsError:
+		print(f'Directory {state_path} already exists, will be preserved')
+	else:
+		print(f'Directory {state_path} created')
+
+	subdir_list = ['context','data','output']
+	for sd in subdir_list:
+		sd_path = f'{state_path}{sd}'
+		try:
+			os.mkdir(sd_path)
+		except FileExistsError:
+			print(f'Directory {sd_path} already exists, will be preserved')
+		else:
+			print(f'Directory {sd_path} created')
+
+
+	ss = sf.State(state_name,state_path)
+	return ss
 
 
 def pick_munger(path_to_munger_dir='../mungers/',column_list=None):
@@ -90,4 +147,16 @@ def new_datafile(raw_file,raw_file_sep,db_paramfile):
 	# TODO can handle datafile
 	munger.check_new_datafile(raw_file,state,new_df_session)
 
+	contest_type_df = pd.DataFrame([
+		['Candidate'], ['Ballot Measure'], ['Both Candidate and Ballot Measure']
+	], columns=['Contest Type'])
+	contest_type_list = pick_one(contest_type_df, item='contest type')
+
 	return
+
+if __name__ == '__main__':
+	raw_file = '../../local_data/NC/data/2018g/nc_general/results_pct_20181106.txt'
+	raw_file_sep = '\t'
+	db_paramfile = '../../local_data/database.ini'
+	new_datafile(raw_file, raw_file_sep, db_paramfile)
+	print('Done! (user_interface)')
