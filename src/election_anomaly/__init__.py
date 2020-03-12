4ca34b3f8679360ca32d6daf8e9fa19617756511
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2020-03-12 15:16:35 -0700

    move enumeration table list def into create_CDF_schema

--- src/election_anomaly/__init__.py
+++ src/election_anomaly/__init__.py
@@ -69,8 +69,8 @@ if __name__ == '__main__':
     if create_db == 'y':
         # create build tables in cdf schema
         print('Creating common data format tables in schema `cdf` in database {}'.format(s.short_name))
-        enumeration_tables = CDF.enum_table_list()
-        meta_cdf = CDF.create_common_data_format_schema(session,'cdf',enumeration_tables,delete_existing=True)
+
+        meta_cdf = CDF.create_common_data_format_schema(session,'cdf',delete_existing=True)
         session.commit()
 
         # load data from context directory into context schema
