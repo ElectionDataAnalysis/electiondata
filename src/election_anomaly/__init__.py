a931318f4570f42c3867737c01656156dbfc7062
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2020-03-29 14:09:17 -0700

    Merge branch 'master' into cleanup
    # Conflicts:
    #	src/election_anomaly/__init__.py
    #	src/election_anomaly/db_routines/Create_CDF_db/__init__.py
    #	src/election_anomaly/db_routines/__init__.py
    #	src/election_anomaly/states_and_files/__init__.py
    #	src/election_anomaly/user_interface/__init__.py
    #	src/jurisdictions/MI/context/Office.txt
    #	src/jurisdictions/MI/context/Party.txt
    #	src/jurisdictions/MI/context/ReportingUnit.txt
    #	src/jurisdictions/MI/context/remark.txt

--- src/election_anomaly/__init__.py
+++ src/election_anomaly/__init__.py
@@ -10,15 +10,12 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
 
 import db_routines as dbr
-import munge_routines as mr
-from db_routines import Create_CDF_db as CDF
 import user_interface as ui
-import states_and_files as sf
 import analyze as an
 
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
-import numpy as np
+import tkinter as tk
 
 try:
     import cPickle as pickle
@@ -26,38 +23,44 @@ except:
     import pickle
 
 if __name__ == '__main__':
-    # print('WARNING: Sorry, lots of bugs at the moment. Don\'t waste your time! -- Stephanie')
-    # exit()
 
-    #project_root = os.getcwd().split('election_anomaly')[0]
-    project_root = Path(__file__).parents[1]
-    # state_short_name = 'NC'
-    state_short_name = None
-    #raw_file = os.path.join(project_root,'local_data/FL/data/11062018Election.txt') # TODO note hard-coded
-    raw_file = os.path.join(project_root, 'local_data', 'MI', 'data', '2018g', 'mi_general', '2018vote.txt')  # TODO note hard-coded
-    raw_file_sep = '\t'
-    #db_paramfile = os.path.join(project_root,'local_data/database.ini')
-    db_paramfile = os.path.join(project_root, 'local_data', 'database.ini')
-
-    s,mu = ui.new_datafile(raw_file,raw_file_sep,db_paramfile,project_root,state_short_name=state_short_name)
-
-    # initialize main session for connecting to db for analysis
-    eng, meta_generic = dbr.sql_alchemy_connect(db_name=s.short_name)
+    # initialize main session for connecting to db for data-loading and analysis
+    eng, meta_generic = dbr.sql_alchemy_connect(db_name=input('Database name?\n'))
     Session = sessionmaker(bind=eng)
     analysis_session = Session()
 
+    # initialize root widget for tkinter to allow picking from  system
+    tk_root = tk.Tk()
+
+    project_root = Path(__file__).parents[1]
+
+    dfile_d,enum_d,raw_file = ui.find_datafile(tk_root,project_root,analysis_session)
+    if enum_d['_datafile_separator'] == 'tab':
+        sep = '\t'
+    elif enum_d['_datafile_separator'] == 'comma':
+        sep = ','
+    else:
+        raise Exception(f'separator {enum_d["_datafile_separator"]} not recognized')
+
+    encoding = dfile_d['encoding']
+    if encoding == '':
+        encoding = 'utf-8'
+
+    s,mu = ui.new_datafile(
+        raw_file,sep,analysis_session,project_root,state_short_name=input('State short_name (e.g., \'NC\'?\n'),
+        encoding=encoding)
     e = an.Election(analysis_session,s)
 
 
     get_top_results = input('Get top-level results (y/n)?\n')
     if get_top_results == 'y':
         top=e.summarize_results(atomic_ru_type=mu.atomic_reporting_unit_type,skip_total_column= not mu.totals_only)
-        print (top)
+        print(top)
 
     get_results_by_vctype = input('Get results by vote type (y/n)?\n')
     if get_results_by_vctype == 'y':
         result=e.summarize_results(atomic_ru_type=mu.atomic_reporting_unit_type,mode='by_vote_type',skip_total_column=~mu.totals_only)
-        print (result)
+        print(result)
 
     need_to_analyze = input('Analyze (y/n)?\n')
     if need_to_analyze == 'y':
@@ -78,16 +81,8 @@ if __name__ == '__main__':
         all_contests = input('Analyze all contests? (y/n)\n')
         if all_contests == 'y':
 
-            pickle_path = e.pickle_dir+'_anomalies_by_'+electionrollup.roll_up_to_ru_type+'_from_'+electionrollup.atomic_ru_type
-            if os.path.isfile(pickle_path):
-                print('Anomalies will not be calculated, but will be read from existing file {}:\n\t'.format(pickle_path))
-                with open(pickle_path,'rb') as f:
-                    anomalies=pickle.load(f)
-            else:
-                anomalies = an.AnomalyDataFrame(electionrollup)
-                with open(pickle_path,'wb') as f:
-                    pickle.dump(anomalies,f)
-                print('AnomalyDataFrame calculated, stored as pickle at '.format(pickle_path))
+            anomalies = an.AnomalyDataFrame(electionrollup)
+            print('AnomalyDataFrame calculated.')
 
             anomalies.worst_bar_for_selected_contests()
             default = 3
