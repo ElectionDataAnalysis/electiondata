5c68eb1367f10bf3ef09160f22a6d6b2cc378962
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2020-05-01 15:47:52 -0700

    warn on bad encoding

--- src/election_anomaly/user_interface/__init__.py
+++ src/election_anomaly/user_interface/__init__.py
@@ -19,6 +19,42 @@ import random
 from tkinter import filedialog
 
 
+recognized_encodings = {'iso2022jp', 'arabic', 'cp861', 'csptcp154', 'shiftjisx0213', '950', 'IBM775',
+						'IBM861', 'shift_jis', 'euc_jp', 'ibm1026', 'ascii', 'IBM437', 'EBCDIC-CP-BE',
+						'csshiftjis', 'cp1253', 'jisx0213', 'latin', 'cp874', '861', 'windows-1255', 'cp1361',
+						'macroman', 'ms950', 'iso-2022-jp-3', 'iso8859_14', 'cp949', 'utf_16', '932', 'cp737',
+						'iso2022_jp_2004', 'ks_c-5601', 'iso-2022-kr', 'ms936', 'cp819', 'iso-8859-3', 'windows-1258',
+						'csiso2022kr', 'iso-8859-2', 'iso2022_jp_ext', 'hz', 'iso-8859-13', 'IBM855', 'cp1140', '866',
+						'862', 'iso2022jp-2004', 'cp1250', 'windows-1254', 'cp1258', 'gb2312-1980', '936', 'L6',
+						'iso-8859-6', 'ms932', 'macgreek', 'cp154', 'big5-tw', 'maccentraleurope', 'iso-8859-7',
+						'ks_x-1001', 'csbig5', 'cp1257', 'latin1', 'mac_roman', 'euckr', 'latin3', 'eucjis2004',
+						'437', 'cp500', 'mac_latin2', 'CP-GR', 'IBM863', 'hz-gb-2312', 'iso2022jp-3', 'iso-8859-15',
+						'koi8_r', 'sjisx0213', 'windows-1252', '850', 'cp855', 'windows1256', 'eucjisx0213', 'hkscs',
+						'gb18030', 'iso-2022-jp-2004', 'L1', 'cyrillic-asian', 'iso2022jp-ext', 'cp1006', 'utf16',
+						'iso2022_kr', 'iso2022jp-2', 'shiftjis', 'IBM037', 'gb2312-80', 'IBM500', '865', 'UTF-16BE',
+						'IBM864', 'EBCDIC-CP-CH', 'iso-8859-4', 'cp856', 'iso2022_jp_1', 'eucjp', 'iso-2022-jp-1',
+						'iso8859_3', 'gb18030-2000', 'cp860', 'mskanji', 'iso2022jp-1', 'iso-8859-8',
+						'iso-2022-jp-ext', 'csiso58gb231280', 'shift_jis_2004', 'L2', 'ms1361', 'cp852', 'ms949',
+						'IBM865', 'cp437', 'iso8859_4', 'iso8859_2', 'cp1255', 'euc_jisx0213', 'cp1252', 'macturkish',
+						'iso8859_9', 'ptcp154', '949', 'cp864', 's_jisx0213', 'big5-hkscs', 'korean', 'iso2022_jp_2',
+						'cp932', 'euc-cn', 'latin5', 'utf_8', 'ibm1140', 'cp862', 'euc_kr', 'iso8859_8', 'iso-8859-9',
+						'utf8', 'cp1251', '863', 'cp850', 'cp857', 'greek', 'latin8', 'iso2022_jp_3', 'iso-8859-10',
+						'big5hkscs', 'ms-kanji', 'iso2022kr', '646', 'iso8859_7', 'koi8_u', 'mac_greek',
+						'windows-1251', 'cp775', 'IBM860', 'u-jis', 'iso-8859-5', 'us-ascii', 'maccyrillic',
+						'IBM866', 'L3', 'sjis2004', 'cp1256', 'sjis_2004', '852', 'windows-1250', 'latin4',
+						'cp037', 'shift_jisx0213', 'greek8', 'latin6', 'latin2', 'mac_turkish', 'IBM862', 'iso8859-1',
+						'cp1026', 'IBM852', 'pt154', 'iso-2022-jp-2', 'ujis', '855', 'iso-8859-14', 'iso-2022-jp',
+						'utf_16_be', 'chinese', 'maclatin2', 'U7', 'hzgb', 'iso8859_5', '857', 'IBM850', '8859',
+						'gb2312', 'cp866', 'CP-IS', 'latin_1', 'L4', 'euccn', 'cyrillic', 'IBM424', 'cp863',
+						'UTF-16LE', 'mac_cyrillic', 'iso8859_10', 'L8', 'IBM869', 'ksc5601', '860', 'iso2022_jp',
+						'hz-gb', 'UTF', 'utf8ascii', 'utf_7', 'cp936', 'euc_jis_2004', 'iso-ir-58', 'csiso2022jp',
+						'IBM039', 'eucgb2312-cn', 'cp950', 'iso8859_13', 'shiftjis2004', 'sjis', 'U8', 'cp1254',
+						's_jis', 'gbk', 'hebrew', 'U16', 'big5', 'cp865', 'cp424', 'uhc', 'windows-1257', '869',
+						'iso-8859-1', 'windows-1253', 'ksx1001', 'johab', 'IBM857', 'L5', 'iso8859_6', 'cp869',
+						'cp875', 'mac_iceland', 'iso8859_15', 'maciceland', 'utf_16_le', 'EBCDIC-CP-HE',
+						'ks_c-5601-1987'}
+
+
 def get_project_root():
 	p_root = os.getcwd().split('election_anomaly')[0]
 	confirmed = False
@@ -42,7 +78,7 @@ def pick_datafile(project_root,sess):
 	print("Locate the datafile in the file system.")
 	fpath = pick_filepath(initialdir=project_root)
 	filename = ntpath.basename(fpath)
-	datafile_record_d, datafile_enumeration_name_d = create_record_in_db(
+	datafile_record_d, datafile_enumeration_name_d = create_record_in_db_NEW(
 		sess,project_root,'_datafile','short_name',known_info_d={'file_name':filename})
 	# TODO typing url into debug window opens the web page; want it to just act like a string
 	return datafile_record_d, datafile_enumeration_name_d, fpath
@@ -54,7 +90,8 @@ def pick_filepath(initialdir='~/'):
 	Returns chosen file path"""
 
 	while True:
-		fpath = input('Enter path to file (or hit return to use pop-up window to find it).\n')
+		fpath = input(
+			'Enter path to file (or hit return to use pop-up window to find it).\n').strip()
 		if not fpath:
 			print('Use pop-up window to pick your file.')
 			fpath = filedialog.askopenfilename(
@@ -672,6 +709,7 @@ def create_record_in_db_NEW(sess,root_dir,table,name_field='Name',known_info_d={
 
 	# read from db and filter
 	all_from_db = pd.read_sql_table(table,sess.bind,index_col='Id')
+	picked_from_db = False
 	if not all_from_db.empty:
 		filtered_from_db = all_from_db.loc[
 			(all_from_db[list(known_info_d)] == pd.Series(known_info_d)).all(axis=1)]
@@ -683,7 +721,6 @@ def create_record_in_db_NEW(sess,root_dir,table,name_field='Name',known_info_d={
 			filtered_from_db_with_plaintext = mr.enum_col_from_id_othertext(filtered_from_db_with_plaintext,e,e_df,drop_old=False)
 
 		# ask user to pick from db
-		picked_from_db = False
 		if not filtered_from_db.empty:
 			print('Is the desired record already in the database?')
 			show_filtered_from_db = filtered_from_db.drop(enum_list,axis=1)
@@ -920,8 +957,12 @@ def new_record_info_from_user(sess,root_dir,table,known_info_d={}):
 			if row["fieldname"] in known_info_d.keys():
 				user_input_record[row["fieldname"]] = known_info_d[row["fieldname"]]
 			else:
+				if 'fieldname' == '_datafile_separator':
+					msg = '\nIf you\'re not sure, try iso-8859-1'
+				else:
+					msg = ''
 				user_input_record[row["fieldname"]] = enter_and_check_datatype(
-					f'Enter the {row["fieldname"]}.',row['datatype'])
+					f'Enter the {row["fieldname"]}.{msg}',row['datatype'])
 
 		for idx,row in df['foreign_keys'].iterrows():
 			target = row['refers_to']
@@ -940,19 +981,21 @@ def new_record_info_from_user(sess,root_dir,table,known_info_d={}):
 				std_enum_list.remove('other')
 				enum_val[e] = user_input_record[f'Other{e}'] = input(f'Enter the {e}:\n')
 				if user_input_record[f'Other{e}'] in std_enum_list:
-					user_input_record[f'{e}_Id'] = enum_df[edf[e].Txt == user_input_record[f'Other{e}']].first_valid_index()
+					user_input_record[f'{e}_Id'] = edf[e][edf[e].Txt == user_input_record[f'Other{e}']].first_valid_index()
 					user_input_record[f'Other{e}'] = ''
 			else:
 				user_input_record[f'Other{e}'] = ''
 				enum_val[e] = enum_txt
-		confirm = input(f'Confirm entry:\n{user_input_record}\nIs this correct (y/n)?\n')
+		entry = [f'{k}:\t{v}\n' for k,v in user_input_record.items()]
+		confirm = input(
+			f'Confirm entry:\n{entry}\nIs this correct (y/n)?\n')
 		if confirm == 'y':
 			unconfirmed = False
 	return user_input_record, enum_val
 
 
 def enter_and_check_datatype(question,datatype):
-	"""Datatype is typically 'Integer', 'String' or 'Date' """
+	"""Datatype is typically 'Integer', 'String', 'Date' or 'Encoding'"""
 	answer = input(f'{question}\n')
 	good = False
 	while not good:
@@ -968,6 +1011,16 @@ def enter_and_check_datatype(question,datatype):
 				good = True
 			except ValueError:
 				answer = input('You need to enter an integer. Try again.')
+		elif datatype == 'Encoding':
+			if answer in recognized_encodings:
+				good = True
+			else:
+				go_on = input(f'This system does not recognize "{answer}" as an encoding. If you are sure it is right,'
+								 f'continue.\nIf you do not know what the encoding is, try "iso-8859-1".\n'
+								 f'For more information, see https://docs.python.org/2.4/lib/standard-encodings.html.\n'
+								 f'Continue with {answer}, even though it is not recognized (y/n)?\n')
+				if go_on == 'y':
+					good = True
 		else:
 			good = True
 	return answer
