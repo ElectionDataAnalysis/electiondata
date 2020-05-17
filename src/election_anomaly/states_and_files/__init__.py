45175c115385b0b53984d5315603008dd9b8a710
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2020-05-17 13:47:24 -0700

    rearrange

--- src/election_anomaly/states_and_files/__init__.py
+++ src/election_anomaly/states_and_files/__init__.py
@@ -12,6 +12,39 @@ import csv
 
 
 class Jurisdiction:
+    def check_element_against_raw_results(self,el,results_df,munger,numerical_columns,d):
+        mode = munger.cdf_elements.loc[el,'source']
+        if mode == 'row':
+            # add munged column
+            raw_fields = [f'{x}_{munger.field_rename_suffix}' for x in munger.cdf_elements.loc[el,'fields']]
+            relevant = results_df[raw_fields].drop_duplicates()
+            mr.add_munged_column(relevant,munger,el,mode=mode)
+        elif mode == 'column':
+            # add munged column
+            formula = munger.cdf_elements.loc[el,'raw_identifier_formula']
+            text_field_list,last_text = mr.text_fragments_and_fields(formula)
+            # TODO create raw element column from formula applied to num col headers
+            val = {}  # holds evaluations of fields
+            raw_val = {}  # holds the raw value for column c
+            for c in numerical_columns:
+                raw_val[c] = ''
+                # evaluate f-th entry in the column whose 0th entry is c
+                for t,f in text_field_list:
+                    if int(f) == 0:  # TODO make prettier, assumes first line is col header in df
+                        val[f] = c
+                    else:
+                        val[f] = results_df.loc[f - 1,c]
+                    raw_val[c] += t + val[f]
+            # TODO check that numerical cols are correctly identified even when more than one header row
+            relevant = pd.DataFrame(pd.Series([raw_val[c] for c in numerical_columns],name=f'{el}_raw'))
+
+        # check for untranslatable items
+        relevant = relevant.merge(
+            d.loc[[el]],how='left',left_on=f'{el}_raw',right_on='raw_identifier_value')
+        missing = relevant[relevant.cdf_internal_name.isnull()]
+
+        return missing
+
     def check_against_raw_results(self,results_df,munger,numerical_columns):
         """Warn user of any mungeable elements in <results_df> that are not
         translatable via context/dictionary.txt"""
@@ -26,40 +59,10 @@ class Jurisdiction:
             problems = []
             # for each relevant element
             for el in munger.cdf_elements.index:
-                mode = munger.cdf_elements.loc[el,'source']
-                if mode in ['row','column']:
-                    # add munged column
-                    if mode == 'row':
-                        raw_fields = [f'{x}_{munger.field_rename_suffix}' for x in munger.cdf_elements.loc[el,'fields']]
-                        relevant = results_df[raw_fields].drop_duplicates()
-                        mr.add_munged_column(relevant,munger,el,mode=mode)
-                    elif mode == 'column':
-                        pass
-                        # TODO
-                        formula = munger.cdf_elements.loc[el,'raw_identifier_formula']
-                        text_field_list,last_text = mr.text_fragments_and_fields(formula)
-                        # TODO create raw element column from formula applied to num col headers
-                        val = {}  # holds evaluations of fields
-                        raw_val = {}  # holds the raw value for column c
-                        for c in numerical_columns:
-                            raw_val[c] = ''
-                            # evaluate f-th entry in the column whose 0th entry is c
-                            for t,f in text_field_list:
-                                if int(f) == 0:  # TODO make prettier, assumes first line is col header in df
-                                    val[f] = c
-                                else:
-                                    val[f] = results_df.loc[f-1,c]
-                                raw_val[c] += t + val[f]
-                        # TODO check that numerical cols are correctly identified even when more than one header row
-                        relevant = pd.DataFrame(pd.Series([raw_val[c] for c in numerical_columns],name=f'{el}_raw'))
-
-                    # check for untranslatable items
-                    relevant = relevant.merge(
-                        d.loc[[el]],how='left',left_on=f'{el}_raw',right_on='raw_identifier_value')
-                    missing = relevant[relevant.cdf_internal_name.isnull()]
-                    if not missing.empty:
-                        ui.show_sample(missing,f'{el}s','cannot be translated')
-                        problems.append(f'At least one {el} unrecognized by dictionary.txt')
+                missing = self.check_element_against_raw_results(el,results_df,munger,numerical_columns,d)
+                if not missing.empty:
+                    ui.show_sample(missing,f'{el}s','cannot be translated')
+                    problems.append(f'At least one {el} unrecognized by dictionary.txt')
             if problems:
                 prob_str = '\n\t'.join(problems)
                 ignore = input(f'Summary of omissions:\n\t{prob_str}\nContinue despite omissions (y/n)?')
@@ -199,7 +202,7 @@ class Munger:
 
             # check encoding
             try:
-                # TODO how best to get separator \t read correction from file?
+                # TODO how best to get separator \t read correctly from file?
                 sep= self.separator.replace('\\t','\t')
                 raw = pd.read_csv(
                     datafile_path,sep=sep,dtype=str,encoding=self.encoding,quoting=csv.QUOTE_MINIMAL,
