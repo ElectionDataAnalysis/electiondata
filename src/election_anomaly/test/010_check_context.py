b7ceb3309672cf35828bdbcf77dd55d60d15c756
Raghu <jsru2@pdx.edu>
2020-05-20 09:25:26 -0700

    removed src/election_anomaly/test from git ignore

---
+++ src/election_anomaly/test/010_check_context.py
@@ -0,0 +1,22 @@
+import states_and_files as sf
+import os
+import user_interface as ui
+
+
+if __name__ == '__main__':
+	print("""Ready to load some election result data?
+			"This program will walk you through the process of creating or checking
+			"an automatic munger that will load your data into a database in the 
+			"NIST common data format.""")
+
+<<<<<<< HEAD
+	project_root = os.path('C:\Users\jsru2\Desktop\GitHub\election_anomaly\src')
+=======
+	project_root = ui.get_project_root()
+>>>>>>> master
+	j_path = os.path.join(project_root,'jurisdictions')
+
+	juris = ui.pick_juris_from_filesystem(project_root,j_path,check_files=True)
+
+	print(f'Context files for {juris.short_name} are internally consistent.')
+	exit()
