16be9d548016715bf9e7d83d3795fa5973150114
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2020-05-19 18:27:15 -0700

    cleanup

--- src/election_anomaly/test/050_load_datafile.py
+++ src/election_anomaly/test/050_load_datafile.py
@@ -11,23 +11,21 @@ if __name__ == '__main__':
 			"an automatic munger that will load your data into a database in the 
 			"NIST common data format.""")
 
-	auto = input('Run interactively (y/n)?\n')
-	if auto != 'y':
+	interact = input('Run interactively (y/n)?\n')
+	if interact == 'y':
+		project_root = ui.get_project_root()
+		juris_name = None
+		db_paramfile = ui.pick_paramfile()
+		db_name = ui.pick_database(project_root,db_paramfile)
+		munger_name = None
+
+	else:
 		d = ui.config(section='050',msg='Pick a paramfile for 050.')
 		project_root = d['project_root']
 		juris_name = d['juris_name']
 		db_paramfile = d['db_paramfile']
 		db_name = d['db_name']
 		munger_name = d['munger_name']
-		data_path = d['data_path']
-
-	else:
-		project_root = ui.get_project_root()
-		juris_name = None
-		db_paramfile = ui.pick_paramfile()
-		db_name = ui.pick_database(project_root,db_paramfile,db_name=db_name)
-		munger_name = None
-		[dfile_d,enum_d,data_path] = ui.pick_datafile(project_root,sess)
 
 	# pick jurisdiction
 	juris = ui.pick_juris_from_filesystem(project_root,juris_name=juris_name)
@@ -37,13 +35,16 @@ if __name__ == '__main__':
 	Session = sessionmaker(bind=eng)
 	sess = Session()
 
+	if interact == 'y':
+		[dfile_d,enum_d,data_path] = ui.pick_datafile(project_root,sess)
+	else:
+		data_path = d['data_path']
+
 	# pick munger
 	munger = ui.pick_munger(mungers_dir=os.path.join(project_root,'mungers'),
 							project_root=project_root,session=sess,munger_name=munger_name)
 
 	# get datafile & info
-
-
 	# load new datafile
 	ui.new_datafile(sess,munger,data_path,juris=juris,project_root=project_root)
 
