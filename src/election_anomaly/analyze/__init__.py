da10fa197912f3b0fa914354a65078fc43d98ba9
Eric Tsai <etsai@treetopllc.com>
2020-07-14 13:49:48 -0700

    rename analyze_via_pandas to analyze

--- src/election_anomaly/analyze/__init__.py
+++ src/election_anomaly/analyze/__init__.py
@@ -252,7 +252,6 @@ def short_name(text,sep=';'):
 		ru_children,left_on='ChildReportingUnit_Id',right_index=True).merge(
 		sub_ru,left_on='ParentReportingUnit_Id',right_index=True,suffixes=['','_Parent'])
 	unsummed.rename(columns={'Name_Parent':'ReportingUnit'},inplace=True)
-
 	# add columns with names
 	unsummed = mr.enum_col_from_id_othertext(unsummed,'CountItemType',df['CountItemType'])
 	unsummed = unsummed.merge(contest_selection,how='left',left_on='ContestSelectionJoin_Id',right_index=True)
