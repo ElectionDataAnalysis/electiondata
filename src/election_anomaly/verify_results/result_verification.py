8ea76be901196a901eb93e620b98421cdcb30ed4
Raghu Srungavarapu <jsru2@pdx.edu>
2020-07-27 05:05:41 -0700

    edge cases considerede

--- src/election_anomaly/verify_results/result_verification.py
+++ src/election_anomaly/verify_results/result_verification.py
@@ -23,81 +23,95 @@ if __name__ == '__main__':
     fpath1 = ui.pick_path()
     TypetotalMod_df = pd.read_csv(fpath1,sep='\t')
 
+    #Check if the files are comparable
+    Typetotal_df_header = list(Typetotal_df.columns)
+    TypetotalMod_df_header = list(TypetotalMod_df.columns)
 
+    Typetotal_df_header.sort()
+    TypetotalMod_df_header.sort()
 
-    #Find rows which are different between two DataFrames.
-    comparison_df = Typetotal_df.merge(TypetotalMod_df, indicator=True, how='outer')
-
-
-    """
-        Case 1: A [contest, reportingunit, Selection] set in both the files and vote counts match. Both files match
-        Case 2: A [contest, reportingunit, Selection] Set in both the files and count not same. Show difference in vote counts.
-        Case 3: A [contest, reportingunit, Selection] pair in in result file 2 but not in result file 1
-        Case 4: A [contest, reportingunit, Selection] pair in result file 1 but not in result file 2 
-    """
-
-    #case 1
-    merge_types = ['left_only','right_only']
-    diff_df = comparison_df[comparison_df._merge.isin(merge_types)]
-    if len(diff_df.index) == 0:
-        print(f'No discrepancies found between \n {fpath} and \n {fpath1}')
+    if Typetotal_df_header != TypetotalMod_df_header:
+        print(' The header rows of both files do not match. The files cannot be compared')
 
     else:
+        # Find rows which are different between two DataFrames.
+        comparison_df = Typetotal_df.merge(TypetotalMod_df, indicator=True, how='outer')
 
-        #case 2
-        #Define coulumns to group by and campare
-        grp_cols = ['Contest', 'ReportingUnit', 'Selection']
-        if 'CountItemType' in comparison_df.columns:
-            grp_cols.append('CountItemType')
-        differences_df = comparison_df.groupby(grp_cols).filter(lambda x: x.Count.count() > 1)
-
-        if len(differences_df.index) == 0:
-            print('Vote counts of all the contests match in both the files')
+        #case 1
+        merge_types = ['left_only','right_only']
+        diff_df = comparison_df[comparison_df._merge.isin(merge_types)]
+        if len(diff_df.index) == 0:
+            print(f'No discrepancies found between \n {fpath} and \n {fpath1}')
         else:
-            # remove duplicate rows and display one row for each contest whose counts vary
 
-            describers = {'_merge': {'left_only': 'Vote Count in Result file 1', 'right_only': 'Vote Count in file 2'}}
-            differences_df = differences_df.replace(describers)
-            differences_df_top = list(differences_df.columns)
-            differences_df_top.remove('Count')
-            differences_df = differences_df.set_index(differences_df_top)
-            differences_df = differences_df.unstack()
+            #case 2
+            #Define coulumns to group by and campare
+            grp_cols = ['Contest', 'ReportingUnit', 'Selection']
+            if 'CountItemType' in comparison_df.columns:
+                grp_cols.append('CountItemType')
+            differences_df = comparison_df.groupby(grp_cols).filter(lambda x: x.Count.count() > 1)
 
-            count = len(differences_df.index)
-            print("Vote counts do not match in {} rows".format(count))
+            if len(differences_df.index) == 0:
+                print('Vote counts of the matching rows from both files are same. Proceed to check the unmatched roes')
+            else:
+                # remove duplicate rows and display one row for each contest whose counts vary
 
-            interact = input('Do you want to look at these rows  (y/n)?\n')
-            if interact == 'y':
-                print(differences_df)
+                """
+                    Case 1: A [contest, reportingunit, Selection] set in both the files and vote counts match. Both files match
+                    Case 2: A [contest, reportingunit, Selection] Set in both the files and count not same. Show difference in vote counts.
+                    Case 3: A [contest, reportingunit, Selection] pair in in result file 2 but not in result file 1
+                    Case 4: A [contest, reportingunit, Selection] pair in result file 1 but not in result file 2 
+                """
 
-            interact = input('Do you want to export the unmatched rows to a file  (y/n)?\n')
-            if interact == 'y':
-                p_root = os.getcwd()
-                print("The default folder for the file is {}".format(p_root))
-                interact2 = input('Do you want to select a different folder(y/n)?\n')
-                if interact2 == 'y':
-                    p_root =ui.pick_path(mode='directory')
+                describers = {'_merge': {'left_only': 'Vote Count in Result file 1', 'right_only': 'Vote Count in file 2'}}
+                differences_df = differences_df.replace(describers)
+                differences_df_top = list(differences_df.columns)
+                differences_df_top.remove('Count')
+                differences_df = differences_df.set_index(differences_df_top)
+                differences_df = differences_df.unstack()
 
-                #Add timestap to download file name
-                timestr = time.strftime("%Y%m%d_%H%M%S")
-                fpath = os.path.join(p_root, 'result_differences_{}.csv'.format(timestr))
-                differences_df.to_csv(fpath, index=True)
+                count = len(differences_df.index)
+                print("Vote counts do not match in {} rows".format(count))
 
-        missing_df = comparison_df.groupby(grp_cols).filter(lambda x: x.Count.count() == 1)
-        describers = {'_merge': {'left_only': 'Row only in file 1', 'right_only': 'Row only in  file 2'}}
-        missing_df = missing_df.replace(describers)
+                interact = input('Do you want to look at these rows  (y/n)?\n')
+                if interact == 'y':
+                    print(differences_df)
 
-        #case3
-        missing1_df = missing_df[missing_df['_merge'] == 'Row only in  file 2']
-        if len(missing1_df.index) != 0:
-            interact = input('Do you want to see the rows in file 2 missing from file 1.(y/n)?\n')
-            if interact == 'y':
-                print(missing1_df)
+                interact = input('Do you want to export the unmatched rows to a file  (y/n)?\n')
+                if interact == 'y':
+                    p_root = os.getcwd()
+                    print("The default folder for the file is {}".format(p_root))
+                    interact2 = input('Do you want to select a different folder(y/n)?\n')
+                    if interact2 == 'y':
+                        p_root =ui.pick_path(mode='directory')
+
+                    #Add timestap to download file name
+                    timestr = time.strftime("%Y%m%d_%H%M%S")
+                    fpath = os.path.join(p_root, 'result_differences_{}.csv'.format(timestr))
+                    differences_df.to_csv(fpath, index=True)
+
+            missing_df = comparison_df.groupby(grp_cols).filter(lambda x: x.Count.count() == 1)
+            describers = {'_merge': {'left_only': 'Row only in file 1', 'right_only': 'Row only in  file 2'}}
+            missing_df = missing_df.replace(describers)
+
+            #case3
+            missing1_df = missing_df[missing_df['_merge'] == 'Row only in  file 2']
+            if len(missing1_df.index) != 0:
+                interact = input('Do you want to see the rows in file 2 missing from file 1.(y/n)?\n')
+                if interact == 'y':
+                    print(missing1_df)
+            else:
+                print('All the rows in file 2 are in file 1')
 
 
-        #case 4
-        missing2_df = missing_df[missing_df['_merge'] == 'Row only in file 1']
-        if len(missing2_df.index) != 0:
-            interact = input('Do you want to see the rows in file 1 missing from file 2.(y/n)?\n')
-            if interact == 'y':
-                print(missing2_df)
+            #case 4
+            missing2_df = missing_df[missing_df['_merge'] == 'Row only in file 1']
+            if len(missing2_df.index) != 0:
+                interact = input('Do you want to see the rows in file 1 missing from file 2.(y/n)?\n')
+                if interact == 'y':
+                    print(missing2_df)
+            else:
+                print('All the rows in file 1 are in file 2')
+                exit()
+
+
