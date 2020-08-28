6747624f01fc6dbaafb8c4d0628781c74f6a3e16
Eric Tsai <etsai@treetopllc.com>
2020-08-28 12:59:19 -0700

    fix margin adjustment calculation

--- src/election_anomaly/analyze/__init__.py
+++ src/election_anomaly/analyze/__init__.py
@@ -341,8 +341,12 @@ def assign_anomaly_score(data):
 	ranked_df = total_data.groupby(['Contest_Id', 'Selection', 'Selection_Id'], as_index=False)['Count'] \
 				.sum().sort_values(['Contest_Id', 'Count'], ascending=False)
 	ranked_df['rank'] = ranked_df.groupby('Contest_Id')['Count'].rank('dense', ascending=False)
-	ranked_df['contest_total'] = ranked_df['Count'].sum()
 	ranked_df.rename(columns={'Count':'ind_total'}, inplace=True)
+
+	# Now get the total votes for the entire contest
+	contest_df = ranked_df.groupby('Contest_Id')['ind_total'].sum().reset_index()
+	contest_df.rename(columns={'ind_total': 'contest_total'}, inplace=True)
+	ranked_df = ranked_df.merge(contest_df, how='inner', on='Contest_Id')
 
 	# Group data by parent info. This works because each child is also its own
 	# parent in the DB table
@@ -404,10 +408,24 @@ def assign_anomaly_score(data):
 
 def get_most_anomalous(data, n):
 	"""gets the n contests with the highest margin ratio score"""
-	margins = list(data['margin_ratio'].unique())
+	data['abs_margins'] = data['margin_ratio'].abs()
+	margins = list(data['abs_margins'].unique())
 	margins.sort(reverse=True)
-	top_margins = margins[0:3]
-	data = data[data['margin_ratio'].isin(top_margins)]
+	top_margins = margins[0:n]
+	data_by_margin = data[data['abs_margins'].isin(top_margins)]
+
+	scores = list(data['score'].unique())
+	scores.sort(reverse=True)
+	top_scores = scores[0:1]
+	unit_id = data[data['score'].isin(top_scores)].iloc[0]['unit_id']
+	data_by_score = data[data['unit_id'] == unit_id]
+
+	if data_by_score.iloc[0]['unit_id'] in set(data_by_margin['unit_id'].unique()):
+		data = data_by_margin
+	else:
+		min_score = data_by_margin['abs_margins'].min()
+		data_by_margin = data_by_margin[data_by_margin['abs_margins'] != min_score]
+		data = pd.concat([data_by_margin, data_by_score])
 
 	zeros_df = data[['Contest_Id', 'ReportingUnitType_Id', 'CountItemType', 'ReportingUnit_Id', 'Count']]
 	zeros_df = zeros_df.groupby(['Contest_Id', 'ReportingUnitType_Id', 'ReportingUnit_Id', 'CountItemType']).sum()
@@ -499,27 +517,58 @@ def calculate_votes_at_stake(data):
 	unit_ids = data['unit_id'].unique()
 	for unit_id in unit_ids:
 		temp_df = data[data['unit_id'] == unit_id]
-		if temp_df.shape[0] > 2:
-			max_margin_score = temp_df['margins_pct'].max() 
-			reporting_unit_id = temp_df[temp_df['margins_pct'] == max_margin_score] \
-				.iloc[0]['ReportingUnit_Id']
-			selection = temp_df.loc[temp_df['margins_pct'] == max_margin_score] \
-				.iloc[0]['Selection']
+		try:
+			# get a df of the most anomalous pairing
+			max_score = temp_df['score'].max()
+			index = temp_df.index[temp_df['score'] == max_score][0]
+			reporting_unit_id = temp_df.loc[index, 'ReportingUnit_Id']
+			selection = temp_df.loc[index, 'Selection']
+			margin_pct = temp_df.loc[index, 'margins_pct']
 			anomalous_df = temp_df[(temp_df['ReportingUnit_Id'] == reporting_unit_id) & \
-				(((temp_df['Selection'] == selection)  & \
-					(temp_df['margins_pct'] == max_margin_score)) |
+				((temp_df['score'] == max_score) |
 				(temp_df['rank'] == 1))].sort_values('rank', ascending=False)
-			next_margin_score = temp_df[(temp_df['margins_pct'] < max_margin_score) &\
-				(temp_df['rank'] != 1)]['margins_pct'].max()
-			next_reporting_unit_id = temp_df.loc[temp_df['margins_pct'] == next_margin_score] \
-				.iloc[0]['ReportingUnit_Id']
-			next_anomalous_df =temp_df[(temp_df['ReportingUnit_Id'] == next_reporting_unit_id) & \
-				(((temp_df['Selection'] == selection)  & \
-					(temp_df['margins_pct'] == next_margin_score)) |
+
+			# get a df of the pairing with closest margin to the most anomalous
+			no_zeros = temp_df[(temp_df['margins_pct'] != 0.0) &
+				(temp_df['margins_pct'] != margin_pct) & 
+				(temp_df['Selection'] == selection)]
+			next_index = no_zeros.iloc[(no_zeros['margins_pct']-margin_pct) \
+				.abs().argsort()[:1]].index[0]
+			next_reporting_unit_id = temp_df.loc[next_index, 'ReportingUnit_Id'] 
+			next_margin_pct = temp_df.loc[next_index, 'margins_pct'] 
+			next_anomalous_df = temp_df[(temp_df['ReportingUnit_Id'] == next_reporting_unit_id) & \
+				((temp_df['margins_pct'] == next_margin_pct) |
 				(temp_df['rank'] == 1))].sort_values('rank', ascending=False)
-			temp_df['margin_ratio'] = (anomalous_df.iloc[0]['margins'] - next_anomalous_df.iloc[0]['margins']) \
-				/ anomalous_df.iloc[0]['margins']
-		else:
+			
+			"""move the most anomalous to the closest and calculate what the 
+			change to the Contest margin would be"""
+
+			# first calculate the Contest margin
+			winner_bucket_total = int(anomalous_df[anomalous_df['rank'] == 1]['Count'])
+			not_winner_bucket_total = int(anomalous_df[anomalous_df['rank'] != 1]['Count'])
+
+			reported_bucket_total = int(anomalous_df['Count'].sum())
+			next_bucket_total = int(next_anomalous_df['Count'].sum()) 
+			adj_margin = next_anomalous_df[next_anomalous_df['rank'] != 1].iloc[0]['Count'] / next_bucket_total
+			not_winner_adj_bucket_total = int(reported_bucket_total * adj_margin)
+			winner_adj_bucket_total = reported_bucket_total - not_winner_adj_bucket_total
+
+			# this does contest margin by raw values
+			contest_margin = winner_bucket_total - not_winner_bucket_total
+			adj_contest_margin = winner_adj_bucket_total - not_winner_adj_bucket_total
+
+			temp_df['margin_ratio'] = (contest_margin - adj_contest_margin) / contest_margin
+
+			# print(temp_df[['Count','Selection','rank','margins','score', 'selection_total', 'ind_total', 'contest_total','CountItemType','Name']])
+			# print('\n')
+			# print(anomalous_df[['Count','Selection','rank','margins','score', 'selection_total', 'ind_total', 'contest_total','CountItemType','Name']])
+			# print('\n')
+			# print(next_anomalous_df[['Count','Selection','rank','margins','score','selection_total', 'ind_total','contest_total','CountItemType','Name']])
+			# print(f"adjusted margin: {adj_contest_margin}, reported margin: {contest_margin}")
+			# print(f"(orig - adjusted) / orig: {temp_df.iloc[0]['margin_ratio']}")
+			# print(f"adj margin: {adj_margin}, winner adj bucket: {winner_adj_bucket_total}, not winner adj bucket: {not_winner_adj_bucket_total}")
+			# input()
+		except:
 			temp_df['margin_ratio'] = 0
 		df = pd.concat([df, temp_df])
 	return df
