fdad9556c23fcbe7689dae7de49147f8efdc8142
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2020-09-03 11:21:12 -0700

    revise process_expressvote()

--- src/election_data_analysis/special_formats/__init__.py
+++ src/election_data_analysis/special_formats/__init__.py
@@ -41,17 +41,15 @@ def process_expressvote(f_path: str, munger: jm.Munger, err: dict) -> (pd.DataFr
     etlc = int(munger.options['empty_top_line_count'])
     v_t_cc = int(munger.options['last_header_column_count'])
 
-    for i in range(etlc-1):
-        # get rid of spurious top lines, leaving one blank line at top
-        data.pop(0)
-
     df = dict()
-    while len(data) > 3:
-        try:
-            # pop the blank line
-            data.pop(0)
 
+    try:
+        while len(data) > 3:
             # TODO allow number & interps of headers to vary?
+            # get rid of blank lines
+            while data[0] == '\n':
+                data.pop(0)
+
             # get the header lines
             header_0 = data.pop(0).strip()
             header_1 = data.pop(0)
@@ -59,52 +57,60 @@ def process_expressvote(f_path: str, munger: jm.Munger, err: dict) -> (pd.DataFr
 
             # get info from header line
             field_list = extract_items(header_line, w)
-            first_column = field_list[0]
             last_header = field_list[1: 1 + v_t_cc]
             assert (len(field_list) - 2) % v_t_cc == 0
 
             header_1_list = extract_items(header_1, w * v_t_cc)
 
-            # TODO get column with header_1 (Candidate, for Georgia)
-            #  create multi-index from the header_1 and votetype, with first index 'county/county'
-            #  and last index 'Total/Total'
-            index_array = [
-                [field_list[0]] + [y for z in [[cand] * v_t_cc for cand in header_1_list] for y in z] + ['Total'],
-                [field_list[0]] + last_header * len(header_1_list) + ['Total']
-            ]
-            multi_index = pd.MultiIndex.from_arrays(index_array)
-
             # create df from next batch of lines, with that multi-index
-            # find idx of next empty line
-            next_empty = next(idx for idx in range(len(data)) if data[idx] == '\n')
-
+            # find idx of next empty line (or end of data)
+            try:
+                next_empty = next(idx for idx in range(len(data)) if data[idx] == '\n')
+            except StopIteration:
+                next_empty = len(data)
             # create io
             vote_count_block = io.StringIO()
             vote_count_block.write(''.join(data[:next_empty]))
             vote_count_block.seek(0)
 
-            df[header_0] = pd.read_fwf(vote_count_block,colspecs='infer',index=False)
-            df[header_0].columns = multi_index
+            df[header_0] = pd.read_fwf(vote_count_block,colspecs='infer',index=False,header=None)
 
-            # Drop total column at far right
-            df[header_0].drop('Total',axis=1,level=1,inplace=True)
+            # Drop column at far right (total over all contests)
+            df[header_0].drop(df[header_0].columns[-1], axis=1, inplace=True)
 
-            # move header_1 & votetype info to columns
+            # make first column into an index
+            df[header_0].set_index(keys=[0],inplace=True)
+
+            # add multi-index with header_1 and header_2 info
+            index_array = [
+                [y for z in [[cand] * v_t_cc for cand in header_1_list] for y in z],
+                last_header * len(header_1_list)
+            ]
+            df[header_0].columns = pd.MultiIndex.from_arrays(index_array)
+
+            # move header_1 & header_2 info to columns
             df[header_0] = pd.melt(
                 df[header_0],
-                col_level=1,
-                id_vars=[first_column],
-                var_name='Count',
-                value_name='header_2'
-            ).rename(columns={first_column:'first_column'})
+                ignore_index=False,
+                value_vars=df[header_0].columns.tolist(),
+                value_name='count',
+                var_name=['header_1','header_2']
+            )
 
             # Add columns for header_0
             df[header_0] = m.add_constant_column(df[header_0],'header_0',header_0)
 
             # remove processed lines from data
             data = data[next_empty:]
-        except Exception as exc:
-            err = ui.add_error(err,"datafile_warning",f"unparsed lines at bottom of file:\n{data}")
+    except Exception as exc:
+        err = ui.add_error(err,"datafile_warning",f"unparsed lines at bottom of file:\n{data}")
+
+    # consolidate all into one dataframe
     raw_results = pd.concat(list(df.values()))
+
+    # Make row index (from first column of blocks) into a column called 'first_column'
+    raw_results.reset_index(inplace=True)
+    raw_results.rename(columns={0:'first_column'},inplace=True)
+
     return raw_results, err
 
