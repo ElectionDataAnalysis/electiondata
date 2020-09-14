00dc90a383c8aa75f3ece84771b1b61c20d85ef8
Eric Tsai <etsai@treetopllc.com>
2020-09-14 10:12:15 -0700

    move setup to v1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="election_data_analysis",
-    version="0.1",
+    version="1.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
