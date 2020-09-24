18e28be15e3caaabc299dc0d420e7b3ce52b8f3f
Stephanie Singer <14353822+sfsinger19103@users.noreply.github.com>
2020-09-24 12:15:27 -0700

    get it running

--- tests/run_multiple_from_repo.py
+++ tests/run_multiple_from_repo.py
@@ -3,12 +3,19 @@ import re
 import datetime
 from pathlib import Path
 import election_data_analysis as e
+from election_data_analysis import database as d
 from election_data_analysis import user_interface as ui
 
-param_file = os.path.join(Path(__file__).parents[1],"src","run_time.ini")
-# create a copy of the git directory in current directory
-cmd = 'git clone https://github.com/ElectionDataAnalysis/TestingData.git'
+reference_param_file = test_param_file = os.path.join(Path(__file__).parents[1], "src", "run_time.ini")
+dataloader_param_file = "run_time.ini"
+
+original_dir = Path(__file__).parent.absolute()
+test_dir = Path(__file__).parent.absolute()
+
+# if there is no testing data directory
 if not os.path.isdir('TestingData'):
+    # create a copy of the git directory in current directory
+    cmd = 'git clone https://github.com/ElectionDataAnalysis/TestingData.git'
     os.system(cmd)
 else:
     print(f"directory TestingData already exists")
@@ -17,44 +24,48 @@ else:
 ts = datetime.datetime.now().strftime("%m%d_%H%M")
 db = f"test_{ts}"
 
-# get runtime parameters from run_time.ini in current directory
-params, err = ui.get_runtime_parameters(
-    required_keys=e.multi_data_loader_pars,
-    param_file=param_file,
-    header="election_data_analysis",
-    err=dict(),
-)
 
-# alter the run_time.ini file to use test database
-with open(param_file, "r") as f:
-    original_params = f.read()
-new_params = re.sub("dbname=[^\n]*\n", f"dbname={db}\n", original_params)
-new_params = re.sub("results_dir=[^\n]*\n", "results_dir=TestingData\n", new_params)
-with open(param_file, "w") as f:
-    f.write(new_params)
+with open(reference_param_file, "r") as f:
+    original_parameter_text = f.read()
+new_parameter_text = re.sub("dbname=[^\n]*\n", f"dbname={db}\n", original_parameter_text)
+new_parameter_text = re.sub("results_dir=[^\n]*\n", "results_dir=TestingData\n", new_parameter_text)
+
+# create local run_time.ini for dataloader, based on existing
+with open(dataloader_param_file, "w") as f:
+    f.write(new_parameter_text)
 
 # Load the data
 dl = e.DataLoader()
 dl.load_all()
 
+# create run_time.ini for testing routines to use
+with open(test_param_file, "w") as f:
+    f.write(new_parameter_text)
+
 # move to tests directory
-original_dir = Path(__file__).parent.absolute()
-test_dir =
-os.system(f"cd {test_dir}")
+os.chdir(test_dir)
 
 # run pytest
 os.system("pytest")
 
 # move back to original directory
-os.system(f"cd {original_dir}")
+os.chdir(original_dir)
 
-# return run_time.ini to its original state
-with open(param_file, "w") as f:
-    f.write(original_params)
+# remove database
+new_params, err = ui.get_runtime_parameters(
+    required_keys=["host","port","user","password","dbname"],
+    param_file=test_param_file,
+    header="postgresql",
+    err=dict(),
+)
+d.remove_database(new_params)
+
+# return run_time.ini to its original state (necessary only when it was in the current directory)
+if reference_param_file == test_param_file:
+    with open(reference_param_file, "w") as f:
+        f.write(original_parameter_text)
 
 # remove testing data
 os.system(f"rm -rf TestingData")
-
-# TODO remove database
 
 exit()
