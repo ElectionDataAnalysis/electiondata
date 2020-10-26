2a21ab62b8344b6c42e5ca2652158d87cf30be3b
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-10-26 12:50:03 -0700

    added Michigan 2020 file

---
+++ tests/test_Michigan_2020-Primary.py
@@ -0,0 +1,32 @@
+import election_data_analysis as e
+
+def test_mi_presidential(dbname):
+    assert (not e.data_exists("2020 Primary","Michigan",dbname=dbname) or e.contest_total(
+            "2020 Primary",
+            "Michigan",
+            "US President (MI)",
+            dbname=dbname,
+        )
+            == 4799284
+    )
+
+
+def test_senate_totals(dbname):
+    assert (not e.data_exists("2020 Primary","Michigan",dbname=dbname) or e.contest_total(
+            "2020 Primary",
+            "Michigan",
+            "MI Senate District 37",
+            dbname=dbname,
+        )
+            == 124414
+    )
+
+def test_house_totals(dbname):
+    assert (not e.data_exists("2020 Primary","Michigan",dbname=dbname) or e.contest_total(
+            "2020 Primary",
+            "Michigan",
+            "MI House District 8",
+            dbname=dbname,
+        )
+            == 28017
+    )
