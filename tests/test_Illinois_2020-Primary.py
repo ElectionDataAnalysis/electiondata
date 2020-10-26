66512cce81f063d85a645ac49141bcd60532ba26
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-10-26 15:46:51 -0700

    added 2020 Illinois primary test file

---
+++ tests/test_Illinois_2020-Primary.py
@@ -0,0 +1,46 @@
+import election_data_analysis as e
+
+def test_data_exists(dbname):
+    assert e.data_exists("2018 General","Colorado",dbname=dbname)
+
+def test_presidential_totals(dbname):
+    assert (e.contest_total(
+            "2020 Primary",
+            "Illinois",
+            "US President (IL) (Democratic Party)",
+            dbname=dbname,
+        )
+            == 2216933
+    )
+
+def test_statewide_totals(dbname):
+    assert (e.contest_total(
+            "2020 Primary",
+            "Illinois",
+            "IL Senate IL (Democratic Party)",
+            dbname=dbname,
+        )
+            == 1941286
+    )
+
+
+def test_senate_totals(dbname):
+    assert (e.contest_total(
+            "2020 Primary",
+            "Illinois",
+            "IL Senate District 11 (Democratic Party)",
+            dbname=dbname,
+        )
+            == 22716
+    )
+
+
+def test_house_totals(dbname):
+    assert (e.contest_total(
+            "2020 Primary",
+            "Illinois",
+            "IL House District 60 (Democratic Party)",
+            dbname=dbname,
+        )
+            == 8888
+    )
