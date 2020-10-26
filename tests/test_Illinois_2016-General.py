4a61b6e353ab276540a7b1db90d5ab0ba9addd59
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-10-26 14:13:10 -0700

    added illinois 2016 general test file

---
+++ tests/test_Illinois_2016-General.py
@@ -0,0 +1,45 @@
+import election_data_analysis as e
+
+
+def test_presidential(dbname):
+    assert (e.contest_total(
+            "2016 General",
+            "Illinois",
+            "US President (IL)",
+            dbname=dbname,
+        )
+            == 5536424
+    )
+
+
+def test_statewide_totals(dbname):
+    assert (e.contest_total(
+            "2016 General",
+            "Illinois",
+            "IL Comptroller",
+            dbname=dbname,
+        )
+            == 5412543
+    )
+
+
+def test_senate_totals(dbname):
+    assert (e.contest_total(
+            "2016 General",
+            "Illinois",
+            "IL Senate District 14",
+            dbname=dbname,
+        )
+            == 79949
+    )
+
+
+def test_house_totals(dbname):
+    assert (e.contest_total(
+            "2016 General",
+            "Illinois",
+            "IL House District 13",
+            dbname=dbname,
+        )
+            == 26712
+    )
