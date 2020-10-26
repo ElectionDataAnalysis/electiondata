ff09834bb3cbc664b3108692f20fa04b0de6574f
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-10-26 12:59:39 -0700

    added party tags

--- tests/test_Michigan_2020-Primary.py
+++ tests/test_Michigan_2020-Primary.py
@@ -4,18 +4,29 @@ def test_mi_presidential(dbname):
     assert (e.contest_total(
             "2020 Primary",
             "Michigan",
-            "US President (MI)",
+            "US President (MI) (Democratic Party)",
             dbname=dbname,
         )
             == 4799284
     )
+
+def test_mi_senate(dbname):
+    assert (e.contest_total(
+            "2020 Primary",
+            "Michigan",
+            "US Senate MI (Democratic Party)",
+            dbname=dbname,
+        )
+            == 1180780
+    )
+
 
 
 def test_senate_totals(dbname):
     assert (e.contest_total(
             "2020 Primary",
             "Michigan",
-            "MI Senate District 37",
+            "MI Senate District 37 (Republican Party)",
             dbname=dbname,
         )
             == 124414
@@ -25,7 +36,7 @@ def test_house_totals(dbname):
     assert (e.contest_total(
             "2020 Primary",
             "Michigan",
-            "MI House District 8",
+            "MI House District 8 (Republican Party)",
             dbname=dbname,
         )
             == 28017
