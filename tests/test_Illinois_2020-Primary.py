357012c93aa2acd270d6d3b391ab560807c375b1
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-10-26 19:05:24 -0700

    updated vote totals

--- tests/test_Illinois_2020-Primary.py
+++ tests/test_Illinois_2020-Primary.py
@@ -10,7 +10,7 @@ def test_presidential_totals(dbname):
             "US President (IL) (Democratic Party)",
             dbname=dbname,
         )
-            == 2216933
+            == 1674133
     )
 
 def test_statewide_totals(dbname):
@@ -20,7 +20,7 @@ def test_statewide_totals(dbname):
             "IL Senate IL (Democratic Party)",
             dbname=dbname,
         )
-            == 1941286
+            == 1446118
     )
 
 
@@ -28,10 +28,10 @@ def test_senate_totals(dbname):
     assert (e.contest_total(
             "2020 Primary",
             "Illinois",
-            "IL Senate District 11 (Democratic Party)",
+            "IL Senate District 31 (Republican Party)",
             dbname=dbname,
         )
-            == 22716
+            == 7219
     )
 
 
@@ -39,8 +39,8 @@ def test_house_totals(dbname):
     assert (e.contest_total(
             "2020 Primary",
             "Illinois",
-            "IL House District 60 (Democratic Party)",
+            "IL House District 100 (Democratic Party)",
             dbname=dbname,
         )
-            == 8888
+            == 9319
     )
