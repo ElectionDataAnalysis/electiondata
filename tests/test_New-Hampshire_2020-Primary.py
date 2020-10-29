472872f293c9380291b23d0df018044eeb57b34e
sfsinger19103 <14353822+sfsinger19103@users.noreply.github.com>
2020-10-28 21:06:53 -0700

    Rep Presidential Primary 2020 munged and successfully tested

--- tests/test_New-Hampshire_2020-Primary.py
+++ tests/test_New-Hampshire_2020-Primary.py
@@ -12,6 +12,16 @@ def test_nh_presidential_20(dbname):
             == 298377 
     )
 
+def test_nh_presidential_20_rep(dbname):
+    assert (e.contest_total(
+            "2020 Primary",
+            "New Hampshire",
+            "US President (NH) (Republican Party)",
+            dbname=dbname,
+        )
+            == 153711
+    )
+
 
 def test_nh_statewide_totals_20(dbname):
     assert (e.contest_total(
@@ -21,6 +31,17 @@ def test_nh_statewide_totals_20(dbname):
             dbname=dbname,
         )
             == 139117
+    )
+
+
+def test_nh_statewide_totals_20_dem(dbname):
+    assert (e.contest_total(
+            "2020 Primary",
+            "New Hampshire",
+            "US Senate NH (Democratic Party)",
+            dbname=dbname,
+        )
+            == 151405
     )
 
 
