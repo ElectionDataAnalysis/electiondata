080068612ac7a5a78cc1813eb51b3c8235ca3c60
ekmeyerson <elliotmeyerson@gmail.com>
2020-10-30 18:03:10 -0700

    add three WI test files

---
+++ tests/test_Wisconsin_2020-Primary.py
@@ -0,0 +1,53 @@
+import election_data_analysis as e
+
+def test_data_exists(dbname):
+    assert e.data_exists("2020 Primary","Wisconsin",dbname=dbname)
+
+
+def test_wi_presidential_totals(dbname):
+    assert (not e.data_exists("2020 Primary","Wisconsin",dbname=dbname) or e.contest_total(
+            "2020 Primary",
+            "Wisconsin",
+            "US President (WI) (Democratic Party)",
+            dbname=dbname,
+        )
+            == 925065
+    )
+
+
+# No results available for additional statewide elections.
+
+
+def test_wi_senate_totals(dbname):
+    assert (not e.data_exists("2020 Primary","Wisconsin",dbname=dbname) or e.contest_total(
+            "2020 Primary",
+            "Wisconsin",
+            "WI Senate District 28 (Republican Party)",
+            dbname=dbname,
+        )
+            == 20309
+    )
+
+
+def test_wi_house_totals(dbname):
+    assert (not e.data_exists("2020 Primary","Wisconsin",dbname=dbname) or e.contest_total(
+            "2020 Primary",
+            "Wisconsin",
+            "WI House District 4 (Democratic Party)",
+            dbname=dbname,
+        )
+            == 4724
+    )
+
+
+def test_us_house_totals(dbname):
+    assert (not e.data_exists("2020 Primary","Wisconsin",dbname=dbname) or e.contest_total(
+            "2020 Primary",
+            "Wisconsin",
+            "US House WI District 1 (Republican Party)",
+            dbname=dbname,
+        )
+            == 40404
+    )
+
+# results not available by vote type
