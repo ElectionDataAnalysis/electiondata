e0d3de3a8b98c1e83ff5020847ee2efe75f9dfff
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-11-03 17:24:25 -0800

    updated the tutorial

--- tests/2020_general_templates/test_Ohio_2020-General.py
+++ tests/2020_general_templates/test_Ohio_2020-General.py
@@ -1,13 +1,11 @@
 import election_data_analysis as e
 
 #OH20g test
-#This file has the tests filled out for presidential as well as both US and state house and senate elections
-#1st district is default for district level positions. This can be changed by the user if deired
-#If an election is not applicable, you can simply delete from the start of the function line
-#to just above the start of the next function line (or end of document if it is the last test)
-#Once you've determined which elections apply to your jurisdiction, All you need to do is change
-#The -1 at the bottom of the function to the value you independantly calculated for that contest.
-#Then move the saved file to the correct jurisdiction folder in the directory above and you can run the test
+# Instructions:
+#   Delete any tests for contest types your state doesn't have in 2020 (e.g., Florida has no US Senate contest)
+#   (Optional) Change district numbers
+#   Replace each '-1' with the correct number calculated from the results file.
+#   Move this testing file to the correct jurisdiction folder in `election_data_analysis/tests`
 
 def data_exists(dbname):
     assert e.data_exists("2020 General","Ohio",dbname=dbname)
