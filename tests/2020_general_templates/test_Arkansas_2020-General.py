0d2c4ac172b9ff3472c6d55433004cee40d26377
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-11-03 16:19:28 -0800

    updated with new contests and added tutorial

--- tests/2020_general_templates/test_Arkansas_2020-General.py
+++ tests/2020_general_templates/test_Arkansas_2020-General.py
@@ -1,6 +1,13 @@
 import election_data_analysis as e
 
 #AR20g test
+#This file has the tests filled out for presidential as well as both US and state house and senate elections
+#1st district is default for district level positions. This can be changed by the user if deired
+#If an election is not applicable, you can simply delete from the start of the function line
+#to just above the start of the next function line (or end of document if it is the last test)
+#Once you've determined which elections apply to your jurisdiction, All you need to do is change
+#The -1 at the bottom of the function to the value you independantly calculated for that contest.
+#Then move the saved file to the correct jurisdiction folder in the directory above and you can run the test
 
 def data_exists(dbname):
     assert e.data_exists("2020 General","Arkansas",dbname=dbname)
@@ -15,21 +22,31 @@ def test_presidential(dbname):
         == -1
     )
 
-def test_congressional_totals(dbname):
-    assert ( e.contest_total(
+def test_senate_totals(dbname):
+    assert (e.contest_total(
         "2020 General",
         "Arkansas",
-        "congressional",
+        "US Senate AR",
+        dbname=dbname,
+        )
+        == -1
+    )
+
+def test_congressional_totals(dbname):
+    assert (e.contest_total(
+        "2020 General",
+        "Arkansas",
+        "US House AR District 1",
         dbname=dbname,
         )
         == -1
     )
 
 def test_state_senate_totals(dbname):
-    assert ( e.contest_total(
+    assert (e.contest_total(
         "2020 General",
         "Arkansas",
-        "state-senate",
+        "AR Senate District 1",
         dbname=dbname,
         )
         == -1
@@ -39,7 +56,7 @@ def test_state_house_totals(dbname):
     assert ( e.contest_total(
         "2020 General",
         "Arkansas",
-        "state-house",
+        "AR House District 1",
         dbname=dbname,
         )
         == -1
