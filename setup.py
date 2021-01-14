df8c0fc06326d1d8e7498044c60dbb05fdbfa580
Eric Tsai <etsai@treetopllc.com>
2021-01-14 10:23:01 -0800

    increment major version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="election_data_analysis",
-    version="1.0",
+    version="2.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
