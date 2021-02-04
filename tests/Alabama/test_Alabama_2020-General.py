40986dc42d6b0fede983bdd17a39bc42266fad14
sfsinger19103 <14353822+sfsinger19103@users.noreply.github.com>
2021-02-04 12:39:36 -0800

    Revert "AR, awaiting resolution of issue 575"
    This reverts commit 39a1ab4b

--- tests/Alabama/test_Alabama_2020-General.py
+++ tests/Alabama/test_Alabama_2020-General.py
@@ -9,14 +9,8 @@ jurisdiction = "Alabama"
 #   Replace each '-1' with the correct number calculated from the results file.
 #   Move this testing file to the correct jurisdiction folder in `election_data_analysis/tests`
 
-# source for official result counts in this file: # source:
-#  https://www.sos.alabama.gov/sites/default/files/election-2020/Final%20Canvass%20of%20Results-Merged.pdf
-
 jurisdiction = "Alabama"
 abbr = "AL"
-single_county = "Alabama;Choctaw County"
-county_or_other = "county"
-pres_votes_county = 3127 + 4296 + 38 + 3
 
 
 def test_data_exists(dbname):
@@ -31,7 +25,7 @@ def test_presidential(dbname):
             f"US President ({abbr})",
             dbname=dbname,
         )
-        == 849624 + 1441170 + 25176 + 7312
+        == 2284601
     )
 
 
@@ -43,7 +37,7 @@ def test_senate_totals(dbname):
             f"US Senate {abbr}",
             dbname=dbname,
         )
-        == 920478 + 1392076 + 3891
+        == 2277854
     )
 
 
@@ -55,7 +49,7 @@ def test_congressional_totals(dbname):
             f"US House {abbr} District 1",
             dbname=dbname,
         )
-        == 116949 + 211825 + 301
+        == 314491
     )
 
 
@@ -68,29 +62,3 @@ def test_all_candidates_known(dbname):
         e.get_contest_with_unknown_candidates(election, jurisdiction, dbname=dbname)
         == []
     )
-
-
-def test_standard_vote_types(dbname):
-    assert e.check_count_types_standard(election, jurisdiction, dbname=dbname)
-
-
-def test_vote_type_counts_consistent(dbname):
-    assert e.check_totals_match_vote_types(election, jurisdiction, dbname=dbname)
-
-
-
-def test_county_subtotal(dbname):
-    assert (
-        e.contest_total(
-            election,
-            jurisdiction,
-            f"US President ({abbr})",
-            dbname=dbname,
-            county=single_county,
-            sub_unit_type=county_or_other,
-        )
-        == pres_votes_county
-    )
-
-
-
