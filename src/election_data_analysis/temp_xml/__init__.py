9873e9dcf901932b7bc4e0ca9249163b6ec81607
sfsinger19103 <14353822+sfsinger19103@users.noreply.github.com>
2021-02-28 14:53:51 -0800

    xml created, not yet validated (WIP)

--- src/election_data_analysis/temp_xml/__init__.py
+++ src/election_data_analysis/temp_xml/__init__.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as et
 import pandas as pd
+from datetime import datetime
 from election_data_analysis import (
     analyze as an,
     database as db,
@@ -9,19 +10,22 @@ def nist_xml_export(session, election, jurisdiction):
     election_id = db.name_to_id(session, "Election", election)
     jurisdiction_id = db.name_to_id(session, "ReportingUnit", jurisdiction)
 
-    root = et.Element("ElectionReport")
+    # create ElectionReport
+    attr = {
+        "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+        "xmlns": "http://itl.nist.gov/ns/voting/1500-100/v2",
+    }
+    root = et.Element("ElectionReport", attr)
     tree = et.ElementTree(root)
+    
 
     # election
     elections = an.nist_election(session, election_id, jurisdiction_id)
     for e in elections:
-        attr = {
-            "objectId": str(e["Id"]),
-            "Date": "1900-01-01",    # TODO placeholder, needs to be fixed
-            "Name": e["Name"],
-            "Type": e["Type"],
-        }
-        e_elt = et.SubElement(root, "Election", attr)
+        e_elt = et.SubElement(root, "Election", {"ObjectId": f'oid{e["Id"]}'})
+        et.SubElement(e_elt, "Date").text = "1900-01-01"  # TODO placeholder, needs to be fixed
+        et.SubElement(e_elt, "Name").text = e["Name"]
+        et.SubElement(e_elt, "Type").text = e["Type"]
 
     # offices
     offices = an.nist_office(session, election_id, jurisdiction_id)
@@ -30,41 +34,42 @@ def nist_xml_export(session, election, jurisdiction):
     for off in offices:
         gpu_ids.add(off["ElectoralDistrictId"])
         attr = {
-            "objectId": str(off["Id"]),
+            "ObjectId": f'oid{off["Id"]}',
             "Name": off["Name"],
-            # "ElectoralDistrictId": str(off["ElectoralDistrictId"]),
         }
         off_elt = et.SubElement(root, "Office", attr)
         ed_id = et.SubElement(off_elt, "ElectoralDistrictId")
-        ed_id.text = str(off["ElectoralDistrictId"])
+        ed_id.text = f'oid{off["ElectoralDistrictId"]}'
 
     # contests
     contests = an.nist_candidate_contest(session, election_id, jurisdiction_id)
     for con in contests:
 
         attr = {
-            "objectId": str(con['Id']),
+            "ObjectId": f'oid{con["Id"]}',
             "Type": con["Type"],
-            "ContestName": con["ContestName"],
         }
-        con_elt = et.SubElement(root, "Contest", attr)
+        con_elt = et.SubElement(e_elt, "Contest", attr)
         off_id_elt = et.SubElement(con_elt, "OfficeId")
-        off_id_elt.text = str(con["OfficeId"])
+        off_id_elt.text = f'oid{con["OfficeId"]}'
+        # add ContestName as a subelement
+        et.SubElement(con_elt, "ContestName").text = con["ContestName"]
+
         for s_dict in con["BallotSelection"]:
             attr = {
-                "objectId": str(s_dict["Id"]),
+                "ObjectId": str(s_dict["Id"]),
                 "Type": "CandidateSelection",
             }
             s_elt = et.SubElement(con_elt, "BallotSelection", attr)
             can_id_elt = et.SubElement(s_elt, "CandidateId")
-            can_id_elt.text = str(s_dict["CandidateId"])
+            can_id_elt.text = f'oid{s_dict["CandidateId"]}'
             vcs_elt = et.SubElement(s_elt, "VoteCountsCollection", dict())
             for vc_dict in s_dict["VoteCounts"]:
                 vc_elt = et.SubElement(vcs_elt, "VoteCounts")
                 gpu_id_elt = et.SubElement(vc_elt, "CountItemType")
                 gpu_id_elt.text = vc_dict["CountItemType"]
                 gpu_id_elt = et.SubElement(vc_elt, "GpUnitId")
-                gpu_id_elt.text = str(vc_dict["GpUnitId"])
+                gpu_id_elt.text = f'oid{vc_dict["GpUnitId"]}'
                 gpu_id_elt = et.SubElement(vc_elt, "Count")
                 gpu_id_elt.text = str(vc_dict["Count"])
 
@@ -85,16 +90,16 @@ def nist_xml_export(session, election, jurisdiction):
             rut = ru_types.loc[rus.loc[idx]["ReportingUnitType_Id"]]["Txt"]
         assert rut != "other", f"ReportingUnit with index {idx} has type other"
         children = [
-            str(x) for x in
+            f'oid{x}' for x in
             cruj[cruj["ParentReportingUnit_Id"] == idx]["ChildReportingUnit_Id"].unique()
             if x in gpu_ids and x != idx
         ]
         attr = {
-            "objectId": str(idx),
+            "ObjectId": f'oid{idx}',
             "Name": name,
             "Type": rut,
         }
-        gpu_elt = et.SubElement(root, "GpUnit", attr)
+        gpu_elt = et.SubElement(e_elt, "GpUnit", attr)
         if children:
             children_elt = et.SubElement(gpu_elt, "ComposingGpUnitIds")
             children_elt.text = " ".join(children)
@@ -103,7 +108,7 @@ def nist_xml_export(session, election, jurisdiction):
     parties = an.nist_party(session, election_id, jurisdiction_id)
     for p in parties:
         attr = {
-            "objectId": str(p["Id"]),
+            "ObjectId": f'oid{p["Id"]}',
             "Name": p["Name"],
         }
         p_elt = et.SubElement(root, "Party", attr)
@@ -112,12 +117,12 @@ def nist_xml_export(session, election, jurisdiction):
     candidates = an.nist_candidate(session, election_id, jurisdiction_id)
     for can in candidates:
         attr = {
-            "objectId": str(can["Id"]),
+            "ObjectId": f'oid{can["Id"]}',
             "BallotName": can["BallotName"],
         }
-        can_elt = et.SubElement(root, "Candidate", attr)
+        can_elt = et.SubElement(e_elt, "Candidate", attr)
         party_id_elt = et.SubElement(can_elt, "PartyId")
-        party_id_elt.text = str(can["PartyId"])
+        party_id_elt.text = f'oid{can["PartyId"]}'
 
     return tree
 
