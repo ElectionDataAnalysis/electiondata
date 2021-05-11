8e1ffcd05116ad7407ccf69d24154ac72464b614
sfsinger19103 <14353822+sfsinger19103@users.noreply.github.com>
2021-05-11 09:17:07 -0700

    correct jurisdictions

--- src/election_data_analysis/munge/__init__.py
+++ src/election_data_analysis/munge/__init__.py
@@ -464,7 +464,7 @@ def replace_raw_with_internal_ids(
         e = (
             f"Warning: Results for {working_unmatched.shape[0]} rows with unmatched {element}s "
             f"will not be loaded to database. "
-            f"These records (raw name, internal name) were found in dictionary.txt, but "
+            f"These records (internal name, raw identifier) were found in dictionary.txt, but "
             f"no corresponding record was found in the {element} table in the database: \n{unmatched_str}"
         )
         error = ui.add_new_error(
